b15e3b3466a411da738b95a60c9ba79cc28fb505
AARON HENAO <aaronhenao27@gmail.com>
2023-03-28 02:17:21 -0500

    creacion clase horarios, definicion de metodos e implementacion

--- model/restaurante.py
+++ model/restaurante.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Restaurante:
 
     def __init(self):
@@ -7,19 +10,48 @@ class Restaurante:
     def registrar_mesero(self, nombre: str):
         self.nombre: str = nombre
 
-    def descanso(self, dias_descanso: str):
-        self.dias_descanso: str = dias_descanso
 
-    def dias_laborales(self, dias_laborados: str):
-        self.dias_laborados: str = dias_laborados
-
-    def meseros(self, num_meseros: int):
+    def mesero(self, num_meseros: int):
         self.num_meseros: int = num_meseros
 
 
 class Mesero:
 
-    def __init__(self, nombre: str, documento: int, correo: str):
+    def __init__(self, nombre: str, documento: int):
         self.nombre: str = nombre
         self.documento: int = documento
-        self.correo: str = correo
+
+    def mesero(self, meseros: list):
+        self.meseros: list = []
+
+
+class Horarios:
+    HORAS_MAXIMAS = 48
+    def __init__(self, documento: Mesero.documento,dias_descanso, horas: int):
+        self.documento: Mesero.documento = documento
+        self.dias_descanso: str = dias_descanso
+        self.horas: int = horas
+
+    def semana(self,dias_semana):
+        self.dias_semana: list= ['lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado', 'domingo']
+
+    def descanso(self, dias_descanso: str):
+        self.dias_descanso: str = dias_descanso
+        for i in range (self.num_meseros):
+            dias_descanso = random.sample(self.dias_semana, 2)
+            self.meseros.append({
+                'nombre': f'Mesero {i + 1}',
+                'dias_descanso': dias_descanso,
+                'horas_trabajo': 0,
+                'horario_semana': {},
+                'horario_diario': {},
+                'horario_descanso': {},
+                'correo': f'mesero{i + 1}@restaurante.com'
+            })
+
+    def dias_laborales(self, dias_laborados: str):
+        self.dias_laborados: str = dias_laborados
+
+
+
+
