be24bc67a26c3b05d0a3c7de10010942b95831be
AARON HENAO <aaronhenao27@gmail.com>
2023-03-28 02:01:52 -0500

    Definición de metodos en la clase Restaurante y creación de la clase Mesero

--- model/restaurante.py
+++ model/restaurante.py
@@ -4,3 +4,22 @@ class Restaurante:
 
         pass
 
+    def registrar_mesero(self, nombre: str):
+        self.nombre: str = nombre
+
+    def descanso(self, dias_descanso: str):
+        self.dias_descanso: str = dias_descanso
+
+    def dias_laborales(self, dias_laborados: str):
+        self.dias_laborados: str = dias_laborados
+
+    def meseros(self, num_meseros: int):
+        self.num_meseros: int = num_meseros
+
+
+class Mesero:
+
+    def __init__(self, nombre: str, documento: int, correo: str):
+        self.nombre: str = nombre
+        self.documento: int = documento
+        self.correo: str = correo
