d84557f240f16f17bf10952bdc56550f25a19781
AARON HENAO <aaronhenao27@gmail.com>
2023-04-27 12:52:30 -0500

    Modificación clase horario y Restaurante

--- model/restaurante.py
+++ model/restaurante.py
@@ -1,59 +1,57 @@
 import random
+from dataclasses import dataclass
+from typing import ClassVar
 
 
 class Restaurante:
 
     def __init(self):
-
         pass
 
-    def registrar_mesero(self, nombre: str, documento: int, meseros: list[tuple]):
-        self.nombre: str = nombre
-        self.documento: int = documento
+    def registrar_mesero(self, meseros: list[tuple]):
         self.meseros: list[tuple] = meseros[(self.nombre, self.documento)]
 
 
-    def mesero(self, num_meseros: int):
-        self.num_meseros: int = num_meseros
-
-
+@dataclass
 class Mesero:
+    nombre: str
+    documento: int
 
-    def __init__(self, nombre: str, documento: int):
+    def agregar_mesero(self, nombre: str, documento: int):
         self.nombre: str = nombre
         self.documento: int = documento
-
-    def mesero(self, meseros: list):
-        self.meseros: list = []
+        Restaurante.registrar_mesero(self.nombre, self.documento)
 
 
-class Horarios:
-    HORAS_MAXIMAS = 48
-    def __init__(self, documento: Mesero.documento,dias_descanso, horas: int):
-        self.documento: Mesero.documento = documento
-        self.dias_descanso: str = dias_descanso
-        self.horas: int = horas
 
-    def semana(self,dias_semana: list):
-        self.dias_semana: list = ['lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado', 'domingo']
 
-    def horario_restaurante(self, horario_servicio:list, horario_almuerzo: tuple, horario_tarde: tuple, horario_cena: tuple):
-        self.horario_servicio: dict = {
-            'lunes': (11, 22),
-            'martes': (11, 22),
-            'miércoles': (11, 22),
-            'jueves': (11, 22),
-            'viernes': (11, 23),
-            'sábado': (11, 23),
-            'domingo': (11, 22),
-        }
-        self.horario_almuerzo: tuple = (12,15)
-        self.horario_tarde: tuple = (15,18)
-        self.horario_comida: tuple = (18, 23)
+@dataclass
+class Horario:
+    DIAS_SEMANA: ClassVar[list[str]] = ["LUNES", "MARTES", "MIÉRCOLES", "JUEVES", "VIERNES", "SÁBADO", "DOMINGO"]
+    HORARIO_RESTAURANTE: ClassVar[dict] = dict
+    var = {"LUNES": (11, 22),
+           "MARTES": (11, 22),
+           "MIÉRCOLES": (11, 22),
+           "JUEVES": (11, 22),
+           "VIERNES": (11, 22),
+           "SÁBADO": (11, 22),
+           "DOMINGO": (11, 22), }
+    TURNO_PARTIDO: tuple = (12, 15, 18, 22)
+    SALIDA_TEMPRANO: tuple = (11, 18)
+    ENTRADA_TARDE: tuple = (15, 22)
+    DESCANSO: ClassVar[list[tuple]] = [("LUNES", "MARTES"), ("MARTES", "MIÉRCOLES"), ("MIÉRCOLES", "JUEVES"),
+                                       ("JUEVES", "VIERNES"), ("VIERNES", "SÁBADO"), ("SÁBADO", "DOMINGO"),
+                                       ("DOMINGO", "LUNES")]
+
+class Tabla:
+
+    def horario_y_meseros(self, tabla_horario: list, DIAS_SEMANA=None):
+        self.tabla_horario: list[[list], [list]] = tabla_horario[[Restaurante.registrar_mesero(self.meseros)], [DIAS_SEMANA]]
+
 
     def descanso(self, dias_descanso: str):
         self.dias_descanso: str = dias_descanso
-        for i in range (self.num_meseros):
+        for i in range(self.num_meseros):
             dias_descanso = random.sample(self.dias_semana, 2)
             self.meseros.append({
                 'nombre': f'Mesero {i + 1}',
@@ -62,7 +60,7 @@ class Horarios:
                 'horario_semana': {},
                 'horario_diario': {},
                 'horario_descanso': {},
-                'correo': f'mesero{  i + 1}@restaurante.com'
+                'correo': f'mesero{i + 1}@restaurante.com'
             })
 
     def apertura(self):
@@ -79,5 +77,3 @@ class Horarios:
             dias_descanso = self.mesero['dias_descanso']
             dias_trabajo = [i for i in self.dias_semana if i not in self.dias_descanso]
             horas_trabajo_semana = 0
-
-
